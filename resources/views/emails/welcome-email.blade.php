54c51a29fa9aa3c0cf791d604ed5219ca28413d5
Shadrack784 <Jeanclush1263@gmail.com>
2023-04-13 16:45:19 +0300

    instagram clone
    added itush gram a laravel project for cloning instagram

---
+++ resources/views/emails/welcome-email.blade.php
@@ -0,0 +1,16 @@
+<x-mail::message>
+  # Welcome To iTushGram
+
+  This is a wide community covering all sorts of people. We are glad you joined us. We hope that this will bw your
+  favourite social media site.
+
+  <!-- <x-mail::button :url="''">
+    Button Text
+  </x-mail::button> -->
+
+
+
+  All the best,<br>
+  CEO iTushGram
+  Shadrack Mutuma
+</x-mail::message>
